dda81d0b6966690d1d94480ab6bc6d28b9c3b450
AhmedSalman2002 <75739628+AhmedSalman2002@users.noreply.github.com>
2020-12-25 20:49:17 +0500

    assignment 9
    9.
    MixUp:
    Given strings a and b, return a single string with a and b separated
    by a space '<a> <b>', except swap the first 2 chars of each string.
    e.g.
      'mix', pod' -> 'pox mid'
      'dog', 'dinner' -> 'dig donner'
      Assume a and b are length 2 or more

---
+++ assignment 9/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        'DECLARATION
+        Dim stra, strb, strc, strd, L1, L2, L3, L4 As String
+
+        'INITIALIZATION
+        stra = ""
+        strb = ""
+        strc = ""
+        strd = ""
+        L1 = ""
+        L2 = ""
+        L3 = ""
+        L4 = ""
+        'INPUT
+        Console.Write("please enter first string: ")
+        stra = Console.ReadLine
+
+        Console.Write("please enter second string: ")
+        strb = Console.ReadLine
+
+        'PROCESS
+        L1 = Left(stra, 2)
+        L2 = Right(strb, 1)
+        strc = L1 & L2
+
+        L3 = Left(strb, 2)
+        L4 = Right(stra, 1)
+        strd = L3 & L4
+
+        'OUTPUT
+        Console.WriteLine("your string is: " & strc & " " & strd)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
