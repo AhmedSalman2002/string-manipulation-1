8debb479771de510c03669be82bae56d854f6719
AhmedSalman2002 <75739628+AhmedSalman2002@users.noreply.github.com>
2020-12-25 20:21:04 +0500

    assignment 7
    7.
    both_ends:
    Input a string s, output a string made of the first 2
    and the last 2 chars of the original string,
    so 'spring' yields 'spng'. However, if the string length
    is less than 2, return string s instead the empty string.

---
+++ assignment 7/Module1.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+
+    Sub Main()
+        'DECLARATION
+        Dim S1, L1, L2 As String
+
+        'INITIALIZATION
+        S1 = ""
+        L1 = ""
+        L2 = ""
+
+        'INPUT
+        Console.Write("please enter first string: ")
+        S1 = Console.ReadLine
+
+       
+        'PROCESS
+        If Len(S1) <= 2 Then
+            Console.WriteLine("Your string is: " & S1)
+
+            Console.ReadKey()
+
+        ElseIf Len(S1) >= 2 Then
+            L1 = Left(S1, 2)
+            L2 = Right(S1, 2)
+            Console.WriteLine("your string is: " & L1 & L2)
+
+            Console.ReadKey()
+
+
+
+
+
+
+
+
+        End If
+
+
+    End Sub
+
+End Module
